adcedc984ca5d2635be23cf2db0a323ba625886b
Saúl Solórzano <hola@saulsolorzano.com>
2021-03-14 19:48:41 -0300

    Dark Mode but not 100% happy with the results

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,39 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
+import DarkMode from "./darkMode";
 
 const Header = ({ siteTitle }) => {
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }
   return (
-  <header className="px-2 md:mx-4 md:px-10 py-2 flex items-center justify-between bg-white dark:bg-dark">
-    <h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 leading-tight hover:text-yellow-700">
-      <Link to="/"><span className="bg-bottom bg-no-repeat bg-header bg-header-pattern">{siteTitle}</span></Link>
-      <small className="block text-sm font-light text-gray-500 dark:text-gray-200">
-        Full-Stack Developer
-      </small>
-    </h1>
-    <nav className="space-x-4">
-      <Link to="/" className="text-gray-900 text-base hover:text-violet-500 dark:text-gray-100 dark:hover:text-dark-light">
-        Home
-      </Link>
-      <Link
-        to="/acerca"
-        className="text-gray-900 text-base hover:text-violet-500 dark:text-gray-100 dark:hover:text-dark-light"
-      >
-        Acerca
-      </Link>
-      {/* <Link to="/proyectos" className="text-gray-900 text-base hover:text-violet-500">Proyectos</Link> */}
-      {/* <Link
-        to="/contact"
-        className="text-gray-900 text-base hover:text-violet-500"
-      >
-        Contacto
-      </Link> */}
-    </nav>
-  </header>
-
-)};
+    <header className="px-2 md:mx-4 md:px-10 py-2 flex items-center justify-between bg-white dark:bg-dark">
+      <h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 leading-tight hover:text-yellow-700">
+        <Link to="/">
+          <span className="bg-bottom bg-no-repeat bg-header bg-header-pattern">
+            {siteTitle}
+          </span>
+        </Link>
+        <small className="block text-sm font-light text-gray-500 dark:text-gray-200">
+          Full-Stack Developer
+        </small>
+      </h1>
+      <nav className="space-x-4 flex items-center">
+        <Link
+          to="/"
+          className="text-gray-900 text-base hover:text-violet-500 dark:text-gray-100 dark:hover:text-dark-light"
+        >
+          Home
+        </Link>
+        <Link
+          to="/acerca"
+          className="text-gray-900 text-base hover:text-violet-500 dark:text-gray-100 dark:hover:text-dark-light"
+        >
+          Acerca
+        </Link>
+        <DarkMode />
+      </nav>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/acerca.js
+++ src/pages/acerca.js
@@ -4,7 +4,6 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-
 const AboutPage = ({ data }) => {
   return (
     <Layout>
